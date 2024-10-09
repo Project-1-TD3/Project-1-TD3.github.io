60ade6e53fb43af393378111d823883f0c0874b7
OSMAN ASSWEH radwan <radwanosman2@gmail.com>
2024-10-09 17:28:56 +0200

    ajouter la fonction sur le bouton du footer

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const footerElement = document.querySelector('footer.footer');
+footer.addEventListener("click", (event) => {
+
+   // todo cet evenement sert a creer nouvelle liste quand tu clique dessus
+  });
