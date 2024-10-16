91e30484a48409aba8d671073918d72895d6858b
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-16 09:32:33 +0200

    Introduce transition.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
-const footerElement = document.querySelector('footer.footer');
-footer.addEventListener("click", (event) => {
+const footerElement = document.querySelector('footer>svg');
+footerElement.addEventListener("click", (event) => {
 
    // todo cet evenement sert a creer nouvelle liste quand tu clique dessus
-   
-   
-  });
+
+});
+
+  const articleElements = document.querySelectorAll("article h3");
+  for (const articleElement of articleElements) {
+   articleElement.addEventListener("click", (event) => {
+         const divElement = event.target.parentElement;
+         const articleParentElement = divElement.parentElement;
+         articleParentElement.classList.toggle("collapsed");
+      })
+  }
